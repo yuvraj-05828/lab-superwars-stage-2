bcb65cfd3a77bcf326d12ea23ececb87c0451023
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2022-11-10 11:36:43 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,58 @@
+const PLAYERS = [
+    "Spiderman",
+    "Captain America",
+    "Wonderwoman",
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
+];
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+    // Create players using for loop
+    // Type your code here
+
+    return detailedPlayers;
+}
+
+// getting random strength
+const getRandomStrength = () => {
+    // Return a random integer (0,100]
+    // Note: You can use Math.random() and Math.ceil()
+}
+
+const buildPlayers = (players, type) => {
+    let fragment = '';
+
+    // Loop through players and accumulate HTML template
+    // depending of type of player(hero|villain)
+    // Type your code here
+
+    return fragment;
+}
+// Display players in HTML
+const viewPlayers = (players) => {
+
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+
+}
+
+window.onload = () => {
+    viewPlayers(initPlayers(PLAYERS));
+}
